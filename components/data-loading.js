7cdefbe15efa1769ef6c600a7420f34e6ccf893e
hendrikbgr <hendrik@whalar.com>
2020-12-09 11:04:18 +0100

    [HNC] - removing layout from data-loading component

--- components/data-loading.js
+++ components/data-loading.js
@@ -1,30 +1,27 @@
 import { Box, Text, Spinner } from '@chakra-ui/core';
-import Layout from './layout';
 
 const DataLoading = () => {
     return (
-        <Layout>
-            <Box
-                flex="1"
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center">
-                <Text color="#ff4545" fontSize="35px" fontWeight="bolder" mt="20">
-                    <Spinner
-                        thickness="4px"
-                        speed="0.65s"
-                        emptyColor="gray.200"
-                        color="#ff4545"
-                        marginRight="5"
-                        top="3"
-                        size="xl"
-                        pos="relative"
-                    />
-                    Data is Loading...
-                </Text>
-            </Box>
-        </Layout>
+        <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center">
+            <Text color="#ff4545" fontSize="35px" fontWeight="bolder" mt="20">
+                <Spinner
+                    thickness="4px"
+                    speed="0.65s"
+                    emptyColor="gray.200"
+                    color="#ff4545"
+                    marginRight="5"
+                    top="3"
+                    size="xl"
+                    pos="relative"
+                />
+                Data is Loading...
+            </Text>
+        </Box>
     );
 };
 
